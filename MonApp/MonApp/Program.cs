89a1ddf80b83696a8a82414450f99af4c5115048
Josselin Dufaud <josselin.dufaud@pm.me>
2018-04-27 21:17:33 +0200

    Traduction

--- MonApp/MonApp/Program.cs
+++ MonApp/MonApp/Program.cs
@@ -6,7 +6,7 @@ namespace MonApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Salut monde!");
         }
     }
 }
